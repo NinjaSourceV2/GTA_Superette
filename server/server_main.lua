daf5c80359500301f2ee1378e80c78721b0ca3b9
Ninja <samir932205@gmail.com>
2021-04-27 00:20:42 +0200

    fix max qty

--- server/server_main.lua
+++ server/server_main.lua
@@ -16,11 +16,16 @@ AddEventHandler("GTASuperette:RecevoirItem", function(quantityItems, nameItem, p
 
 	TriggerEvent('GTA:GetUserQtyItem', source, "Argent-Propre", function(argentPropreQty)
 		local cash = argentPropreQty
-
 		if (tonumber(cash) >= prixTotal) then
-			TriggerClientEvent("GTASuperette:Achat", source, quantityItems, nameItem)
-			TriggerClientEvent('nMenuNotif:showNotification', source, " + "..quantityItems .. " ".. nameItem)
-			TriggerEvent('GTA:RetirerArgentPropre', source, tonumber(prixTotal))
+			MySQL.Async.fetchAll("SELECT * FROM items WHERE libelle = @libelle", { ['@libelle'] = nameItem}, function(res)
+				if(res[1]) then
+					TriggerClientEvent("GTASuperette:Achat", source, quantityItems, nameItem, res[1].max_qty)
+					TriggerClientEvent('nMenuNotif:showNotification', source, " + "..quantityItems .. " ".. nameItem)
+					TriggerEvent('GTA:RetirerArgentPropre', source, tonumber(prixTotal))
+				else
+					TriggerClientEvent("GTA_NUI_ShowNotif_client", source, "L'item saisit : "..nameItem.." est introuvable.", "error", "fa fa-exclamation-circle fa-2x")
+				end
+			end)
 		else
 			TriggerClientEvent('GTASuperette:AchatFail', source)
 			TriggerClientEvent('nMenuNotif:showNotification', source, "~r~Tu n'as pas suffisamment d'argent !")
